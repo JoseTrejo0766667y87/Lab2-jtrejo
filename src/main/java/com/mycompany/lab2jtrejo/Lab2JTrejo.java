1072f53633bc7ca36366f1ab008cda6bb13ed755
JoseTrejo0766667y87 <jtrejoc3@miumg.edu.gt>
2026-04-18 17:39:44 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/lab2jtrejo/Lab2JTrejo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab2jtrejo;
+
+/**
+ *
+ * @author umg
+ */
+public class Lab2JTrejo {
+
+    public static void main(String[] args) {
+        Login lg = new Login();
+        
+        lg.setVisible(true);
+    
+        
+    }
+}
